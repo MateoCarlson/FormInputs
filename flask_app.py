013073bd3fb94f4c611cc56aa720e80c8593bccd
Mateo Carlson <mateo.carlson1016@gmail.com>
2018-12-13 23:32:11 +0000

    Started logic

--- flask_app.py
+++ flask_app.py
@@ -17,38 +17,36 @@ app = Flask(__name__)
 #add stationary points
 
 
-"""
 def select_house(playstyle, movement):
     # create dictionary mapping house names to point values
     classes = { "Damage": 0, "Tank": 0, "Support": 0 }
     style = { "Movement" : 0, "Stationary" : 0 }
 
-    # add points to each house based on how their favorite spell aligns
-    if playstyle == "Agressive":
+    # add points to each class based on how their favorite playstyle aligns
+    if playstyle == "Full on agression, no holding back.":
         classes["Damage"] += 1
-    elif playstyle == "Passive Agressive":
+    elif playstyle == "Let the enemy come to you, then crush them.":
         classes["Damage"] += 2
-    elif playstyle == "Clutch":
+    elif playstyle == "Wait for the right moment, then save your team.":
         classes["Tank"] += 3
-    elif playstyle == "Very Passive":
+    elif playstyle == "Stay in the back, and let others do your dirty work.":
         classes["Support"] += 4
 
-    # add points to each house based on how their potion aligns
-    if movement == "Faster moving/ Movement based characters":
+    # add points to each type based on how their potion aligns
+    if movement == "Constant motion, no down time.":
         style["Movement"] += 1
-    elif movement == "Slower moving/ Stationary characters":
+    elif movement == "Slow and methodical":
         style["Stationary"] += 2
 
 
-    # choose the house with the highest value
+    # choose the hero with the highest value
     selected_hero = None
     for hero, points in classes.items():
         if selected_hero is None or classes[selected_hero] < points:
-            selected_hero = house
+            selected_hero = hero
 
-    # return the selected house
+    # return the selected hero
     return selected_hero
-"""
 
 
 @app.route('/', methods=['GET'])
@@ -59,12 +57,11 @@ def main():
 @app.route('/process_inputs', methods=['POST'])
 def process_inputs():
     name = request.form.get('input_name', '')
-    dropdown = request.form.get('input_dropdown', '')
-    select = request.form.get('input_select', '')
-    freeform = request.form.get('input_freeform', '')
+    playstyle = request.form.get('input_playstyle', '')
+    movement = request.form.get('input_select', '')
     if name is "":
-        return render_template("main_page.html", input_data=dropdown,
+        return render_template("main_page.html", group = 1,
                            output="You did not input a name %s, but you should consider playing:" % name)
     else:
-        return render_template("main_page.html", input_data=dropdown,
-                           output="%s, you should consider playing:" % name)
+        return render_template("main_page.html", group = 1,
+                           output= "Hello %s, you should play:" % name)
